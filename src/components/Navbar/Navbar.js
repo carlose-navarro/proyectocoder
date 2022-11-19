bbba4c5234ce412baa7a29c3811b7c4061666828
Carlos Navarro <carlos.navarro@claro.com.ar>
2022-11-19 12:36:15 -0300

    clase de jsx

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,13 @@
+import './Navbar.css'
+
+const Navbar = () => {
+    return(
+        <nav className='navbar'>
+            <h1>Tienda</h1>
+            <button className='boton'>Inicio</button>
+            <button className='boton'>Contacto</button>
+        </nav>
+    )
+}
+
+export default Navbar
